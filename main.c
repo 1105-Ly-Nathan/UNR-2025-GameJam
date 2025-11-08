bdd1155629fb048ba3661c4398621b31123188d7
Nathan Ly <omnirivani@gmail.com>
2025-11-07 20:00:05 -0800

    first commit

---
+++ main.c
@@ -0,0 +1,32 @@
+// walker.c - top-down walker
+#include "raylib.h"
+
+int main(void) {
+    InitWindow(800, 600, "Top-down walker - WASD");
+    SetTargetFPS(60);
+
+    Vector2 pos = {400, 300};
+    float speed = 200.0f;
+
+    while (!WindowShouldClose()) {
+        float dt = GetFrameTime();
+
+        if (IsKeyDown(KEY_W)) pos.y -= speed * dt;
+        if (IsKeyDown(KEY_S)) pos.y += speed * dt;
+        if (IsKeyDown(KEY_A)) pos.x -= speed * dt;
+        if (IsKeyDown(KEY_D)) pos.x += speed * dt;
+
+        if (IsKeyDown(KEY_SPACE)) {
+            DrawText("TESTING", 30, 30, 30, RED);
+        
+        }
+
+        BeginDrawing();
+        ClearBackground(DARKGREEN);
+        DrawCircleV(pos, 20, RED);
+        DrawText("WASD to move", 10, 10, 20, WHITE);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
