63768eaec7270b6c34f8e5a6592ae2af4db02a8a
Nathan Ly <omnirivani@gmail.com>
2025-11-07 22:51:23 -0800

    Initial automovement

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 // walker.c - top-down walker
 #include "raylib.h"
 
+void AutoMove(Vector2 *pos, Vector2 *vel, float speed, float dt, int screenwidth, int screenheight);
+
 int main(void) {
     InitWindow(800, 600, "Top-down walker - WASD");
     SetTargetFPS(60);
 
-    Vector2 pos = {400, 300};
+    int screenwidth = GetScreenWidth();
+    int screenheight = GetScreenHeight();
+
+    // Controls character position (float x, float y)
+    Vector2 pos = {screenwidth/2, screenheight*0.8};
+
+    // Controls character direction, where {-1.0f, 0.0f} points left
+    Vector2 vel = {1.0f, 0.0f};
     float speed = 200.0f;
 
     while (!WindowShouldClose()) {
@@ -16,11 +25,15 @@ int main(void) {
         if (IsKeyDown(KEY_A)) pos.x -= speed * dt;
         if (IsKeyDown(KEY_D)) pos.x += speed * dt;
 
+        // Auto movement
+        AutoMove(&pos, &vel, speed, dt, screenwidth, screenheight);
+
+
         if (IsKeyDown(KEY_SPACE)) {
             DrawText("TESTING", 30, 30, 50, BLUE);
             DrawText("100 100 HERE", 100, 100, 50, RED);
-        
         }
+
 
         BeginDrawing();
         ClearBackground(DARKGREEN);
@@ -30,4 +43,23 @@ int main(void) {
     }
     CloseWindow();
     return 0;
+}
+
+void AutoMove(Vector2 *pos, Vector2 *vel, float speed, float dt, int screenwidth, int screenheight) {
+    // We have position's address, or pointer, as input. Get the pos struct, then access x member
+    // Or alternatively, shorthand. Follow pointer to its destination, getting that value. Leaving here for learning purposes.
+    // Move the character right, then bounce off the wall and back
+    (*pos).x += vel->x * speed * dt;
+
+    // Bounce off Right wall
+    if (pos->x > screenwidth) {
+        pos->x = screenwidth;
+        vel->x = -1.0;
+    }
+    // Bounce off left
+    else if (pos->x < 0) {
+        pos ->x = 0;
+        vel->x = 1.0;
+    }
+    
 }
