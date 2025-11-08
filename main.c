38ddd49b905e38e98426b2cbe8c8f25058581a44
Nathan Ly <omnirivani@gmail.com>
2025-11-08 11:12:53 -0800

    Randomize enemy movement

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
-// walker.c - top-down walker
+
 #include "raylib.h"
 
+#define MAX_ENEMIES 10
+
+// Create enemy class
+typedef struct {
+    Vector2 pos;
+    Vector2 vel;
+    Vector2 accel;
+    float speed;
+    bool alive;
+} Enemy;
+
 void AutoMove(Vector2 *pos, Vector2 *vel, float speed, float dt, int screenwidth, int screenheight);
+void EnemiesMove(Enemy enemies[], float dt, int screenwidth, int screenheight);
 
 int main(void) {
     InitWindow(800, 600, "WASD to move");
@@ -19,12 +31,25 @@ int main(void) {
     // Speed of character
     float speed = 200.0f;
 
-    // Create enemy class
-    typedef struct {
-        Vector2 pos;
-        Vector2 vel;
-        Vector2 accel;
-    } Enemy;
+    // Store enemies into array
+    Enemy enemies[MAX_ENEMIES];
+
+    // Randomize enemy stats
+    for (int i=0; i<MAX_ENEMIES; i++) {
+        enemies[i].pos = (Vector2) {GetRandomValue(0, screenwidth), GetRandomValue(0, screenheight*0.7f)};
+
+        // Cant randomize velocity direction between -1 and 1 (could get 0). Instead, convert 2 outcomes
+        float dirx;
+        if (GetRandomValue(0, 1) == 0) {
+            dirx = -1;
+        }
+        else {
+            dirx = 1;
+        }
+        enemies[i].vel = (Vector2) {dirx, 0};
+        enemies[i].speed = GetRandomValue(100, 300);
+    }
+
 
 
 
@@ -38,7 +63,7 @@ int main(void) {
 
         // Auto movement
         AutoMove(&pos, &vel, speed, dt, screenwidth, screenheight);
-
+        EnemiesMove(enemies, dt, screenwidth, screenheight);
 
         if (IsKeyDown(KEY_SPACE)) {
             DrawText("TESTING", 30, 30, 50, BLUE);
@@ -49,6 +74,10 @@ int main(void) {
         BeginDrawing();
         ClearBackground(DARKGREEN);
         DrawCircleV(pos, 20, RED);
+        // Draw all enemies
+        for (int i=0; i<MAX_ENEMIES; i++) {
+            DrawCircleV(enemies[i].pos, 10, BLUE);
+        }
         DrawText("WASD to move", 10, 10, 20, WHITE);
         EndDrawing();
     }
@@ -72,5 +101,19 @@ void AutoMove(Vector2 *pos, Vector2 *vel, float speed, float dt, int screenwidth
         pos ->x = 0;
         vel->x = 1.0;
     }
-    
+}
+
+void EnemiesMove(Enemy enemies[], float dt, int screenwidth, int screenheight) {
+    for (int i=0; i<MAX_ENEMIES; i++) {
+        enemies[i].pos.x += enemies[i].vel.x * enemies[i].speed * dt;
+        // Bounce back 
+        if (enemies[i].pos.x > screenwidth) {
+            enemies[i].pos.x = screenwidth;
+            enemies[i].vel.x = -1.0;
+        }
+        else if (enemies[i].pos.x < 0) {
+            enemies[i].pos.x = 0;
+            enemies[i].vel.x = 1.0;
+        }
+    }
 }
